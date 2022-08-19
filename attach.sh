26bca90ffbd6e9c6f14e11f3d3db42462185bd0f
SangminLee <sangmin@tendopay.ph>
2022-08-19 10:14:33 -0400

    Add able to login as root

--- attach.sh
+++ attach.sh
@@ -2,7 +2,9 @@
 # https://opensource.com/article/19/9/linux-terminal-colors
 
 CONTAINER=php
+USER=""
 
-[ "$1" != "" ] && CONTAINER=$1
+[ "$1" != "" ] && CONTAINER="$1"
+[ "$2" != "" ] && USER="--user $2"
 
-docker exec -it $CONTAINER /bin/bash --init-file ~/.bashrc_docker
+docker exec -it $USER $CONTAINER /bin/bash --init-file ~/.bashrc_docker
