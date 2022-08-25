a4f4b4aaf898f3da719035db169841fe4a19033e
SangminLee <sangmin@tendopay.ph>
2022-08-25 07:51:33 -0400

    update

--- attach.sh
+++ attach.sh
@@ -2,9 +2,9 @@
 # https://opensource.com/article/19/9/linux-terminal-colors
 
 CONTAINER=php
-USER=""
+OPT="-it"
 
 [ "$1" != "" ] && CONTAINER="$1"
-[ "$2" != "" ] && USER="--user $2"
+[ "$2" != "" ] && OPT="$OPT --user $2"
 
-docker exec -it $USER $CONTAINER /bin/bash --init-file ~/.bashrc_docker
+docker exec -it $OPT $CONTAINER /bin/bash --init-file ~/.bashrc_docker
