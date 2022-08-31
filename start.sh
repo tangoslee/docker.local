67c4cfa009a9357c4231060d09d9ff09713afccd
SangminLee <sangmin@tendopay.ph>
2022-08-31 07:36:55 -0400

    Add vm.overcommit_memory = 1 on /etc/sysctl.d/90.docker.conf

--- start.sh
+++ start.sh
@@ -6,6 +6,12 @@ OPT="$1"
 #sudo sysctl -w vm.max_map_count=262144
 sysctl vm.max_map_count
 
+# WARNING overcommit_memory is set to 0! Background save may fail under low memory condition.
+# To fix this issue add 'vm.overcommit_memory = 1' to /etc/sysctl.conf and then reboot or
+# run the command 'sysctl vm.overcommit_memory=1' for this to take effect.
+sysctl vm.overcommit_memory
+
+
 sudo systemctl start docker.socket && {
   sudo systemctl start docker.service
   docker-compose down
